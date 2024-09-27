27913e8678cc7452deef857e77564160c4e40789
ShivaniGupta <vanigupta2428@gmail.com>
2024-09-27 14:30:01 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -15,28 +15,3 @@ class Solution:
             prev_value = current_value
         
         return total
-
-import unittest
-
-class TestSolution(unittest.TestCase):
-    def setUp(self):
-        self.solution = Solution()
-
-    def test_single_numerals(self):
-        self.assertEqual(self.solution.romanToInt("I"), 1)
-        self.assertEqual(self.solution.romanToInt("V"), 5)
-        self.assertEqual(self.solution.romanToInt("X"), 10)
-
-    def test_complex_numerals(self):
-        self.assertEqual(self.solution.romanToInt("III"), 3)
-        self.assertEqual(self.solution.romanToInt("IV"), 4)
-        self.assertEqual(self.solution.romanToInt("IX"), 9)
-        self.assertEqual(self.solution.romanToInt("LVIII"), 58)
-        self.assertEqual(self.solution.romanToInt("MCMXCIV"), 1994)
-
-    def test_large_numerals(self):
-        self.assertEqual(self.solution.romanToInt("MMMDCCCLXXXVIII"), 3888)
-        self.assertEqual(self.solution.romanToInt("MMMCMXCIX"), 3999)
-
-if __name__ == '__main__':
-    unittest.main(argv=['first-arg-is-ignored'], exit=False)
