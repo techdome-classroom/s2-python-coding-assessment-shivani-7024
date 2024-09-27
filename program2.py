a992da2ecef74503503384f7b846a327cadc4f82
ShivaniGupta <vanigupta2428@gmail.com>
2024-09-27 14:05:18 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,22 +1,17 @@
-# Solution class to convert Roman numerals to integers
 class Solution:
     def romanToInt(self, s: str) -> int:
-        # Mapping of Roman numerals to their corresponding integer values
         roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         total = 0
         prev_value = 0
         
-        # Iterate through the string in reverse (from right to left)
         for char in reversed(s):
             current_value = roman_map[char]
             
-            # If the current value is less than the previous value, subtract it
             if current_value < prev_value:
                 total -= current_value
             else:
                 total += current_value
             
-            # Update the previous value for the next iteration
             prev_value = current_value
         
         return total
